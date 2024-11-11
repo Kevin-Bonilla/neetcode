3fb28ed83093c1aad166fc8c982e0c24ff9648a9
Kevin-Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2024-11-11 13:35:46 -0600

    Meeting Schedule solution added

---
+++ src/neetCode150/Interval.java
@@ -0,0 +1,9 @@
+package neetCode150;
+
+public class Interval {
+	      public int start, end;
+	      public Interval(int start, int end) {
+	          this.start = start;
+	          this.end = end;
+	      }
+}
