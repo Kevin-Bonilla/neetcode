c3f7bcbac454c42d3924c9fef9fcd8e638b7ecf6
Kevin-Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2025-11-19 19:47:51 -0600

    plusone.java solution

---
+++ src/neetCode150/main.java
@@ -0,0 +1,12 @@
+package neetCode150;
+
+public class main {
+
+    public static void main(String[] args){
+        // PlusOne Testing
+        int[] digits = {1,0,0,1};
+
+        PlusOne plusOneClass = new PlusOne();
+        int[] plusOneResult = plusOneClass.plusOne(digits);
+    }
+}
