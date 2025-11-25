f5793914c815a9a3e30f6cd7ca8f93e349f18d14
Kevin-Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2025-11-24 21:03:25 -0600

    ThreeSum solution added

--- src/neetCode150/main.java
+++ src/neetCode150/main.java
@@ -1,5 +1,7 @@
 package neetCode150;
 
+import java.util.List;
+
 public class main {
 
     public static void main(String[] args){
@@ -8,5 +10,10 @@ public class main {
 
         PlusOne plusOneClass = new PlusOne();
         int[] plusOneResult = plusOneClass.plusOne(digits);
+
+        // ThreeSum testing
+        int[] nums = {-1,0,1,2,-1,-4};
+        ThreeSum threeSumClass = new ThreeSum();
+        List<List<Integer>> resultList = threeSumClass.threeSumTwoPointers(nums);
     }
 }
