15937a2b4d6c9b7506866ded308e3d07d1c516e5
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2025-12-09 20:23:05 -0600

    Design singly linked list problem WIP pt 2 maybe

--- src/CoreSkills/LinkedList.java
+++ src/CoreSkills/LinkedList.java
@@ -37,26 +37,114 @@ Note:
 
  */
 
+import java.util.ArrayList;
+
 public class LinkedList {
 
-    public LinkedList() {
+    // nested class for our Node
+    private static class Node {
+        int val;
+        Node next;
 
+        Node(int val) {
+            this.val = val;
+            this.next = null;
+        }
+
+        Node(int val, Node next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LinkedList() {
+        // LinkedList() will initialize an empty linked list.
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
     }
 
     public int get(int index) {
+        // int get(int i) will return the value of the ith node (0-indexed). If the index is out of bounds, return -1.
 
+        // if the linked list is empty
+        if (head == null){
+            return -1;
+        }
+
+        // traverse the linked list to the index
+        int indexCounter = 0;
+        Node currentNode = head;
+        while(currentNode != null){
+            if (indexCounter == index){
+                return currentNode.val;
+            }
+
+            currentNode = currentNode.next;
+            indexCounter++;
+        }
+
+        // if we went out of bounds and exited the while loop before finding the passed in index
+        return -1;
     }
 
     public void insertHead(int val) {
-
+        this.head = new Node(val, head);
+        // if this is the only node in the linked list
+        if (size == 0){
+            this.tail = head;
+        }
+        this.size++;
     }
 
     public void insertTail(int val) {
-
+        Node newTail = new Node(val);
+        this.tail.next = newTail;
+        this.tail = newTail;
+        // if only node in the linked list
+        if (size == 0){
+            this.head = tail;
+        }
+        this.size++;
     }
 
     public boolean remove(int index) {
+        // bool remove(int i) will remove the ith node (0-indexed). If the index is out of bounds, return false, otherwise return true.
 
+        // if the linked list is empty
+        if (head == null){
+            return false;
+        }
+
+        // case for removing head
+        if (index == 0){
+            this.head = head.next;
+            // if our previous head was the only node in the linked list
+            if(head == null){
+                tail = null;
+            }
+            return true;
+        }
+
+        // TODO: implement this correctly
+        // traverse the linked list to the index
+        int indexCounter = 0;
+        Node currentNode = head;
+        while(currentNode != null){
+            if (indexCounter == index){
+                return currentNode.val;
+            }
+
+            currentNode = currentNode.next;
+            indexCounter++;
+        }
+
+        // if we went out of bounds and exited the while loop before finding the passed in index
+        return -1;
     }
 
     public ArrayList<Integer> getValues() {
