20f422634c25833cd028bb34702cf8295be4b807
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-01-26 20:09:51 -0600

    MergeSortedArray solution wip 2

--- src/neetCode250/MergeSortedArray.java
+++ src/neetCode250/MergeSortedArray.java
@@ -40,12 +40,45 @@ Constraints:
 public class MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //nums1 is our big array that everything needs to be sorted into
-        //nums 2 is our small array that everything needs to be added into nums1 and sorted
+        // nums1 is our big array that everything needs to be sorted into
+        // nums2 is our small array that everything needs to be added into nums1 and sorted
         // m is our valid amount of elements in nums1
         // n is our amount of elements in nums2
 
         // this is a two pointer problem so we should think of it in that way
+
+        // we want to start the merge backwards because there is empty spots at the end that we can override with no consequence
+        // we are going to use three pointers, starting at end of nums 1, ends of nums 2, and our write position (m+n -1)
+
+        int num1Pointer = m - 1;
+        int num2Pointer = n - 1;
+        int writePos = m + n - 1;
+
+        // we loop through, comparing num1Pointer vs num2Pointer, write the greater of the two in the writePos
+        // whichever we wrote from we move the pointer back 1.
+        while (num1Pointer >= 0 && num2Pointer >= 0){
+            int num1Digit = nums1[num1Pointer];
+            int num2Digit = nums2[num2Pointer];
+
+            if (num2Digit >= num1Digit){
+                // if they're equal we just take num2Digit
+                // and we move num2pointer back 1
+                nums1[writePos] = num2Digit;
+                num2Pointer--;
+            }
+            else{
+                // if num1Digit is greater than num2Digit
+                // write our digit and move the num1pointer back 1
+                nums1[writePos] = num1Digit;
+                num1Pointer--;
+            }
+
+        }
+
+        // if num2Pointer is not at 0 by the time we go through num1Pointer, then we just copy the rest of num2s digits
+        // if num2Pointer goes to -1 and we still haven't traversed through num1, its fine because we are already sorted at that point
+
+
 
     }
 
