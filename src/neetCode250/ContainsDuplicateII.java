41d4a4bf5091154494f2b122a775c61e0303086d
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-02-01 19:17:19 -0600

    ContainsDuplicateII Solution Completed

--- src/neetCode250/ContainsDuplicateII.java
+++ src/neetCode250/ContainsDuplicateII.java
@@ -26,9 +26,38 @@ Constraints:
     0 <= k <= 100,000
  */
 
+import java.util.HashSet;
+
 public class ContainsDuplicateII {
     // this is a sliding window problem
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // for this question k is the max distance the two duplicate values can be apart.
+        // if a pair of dupes does not meet this req then we are returning false
 
+        // our sliding window is going to have a size of k + 1
+        // we are going to use a hashset to represent our sliding windows data
+        HashSet<Integer> slidingWindow = new HashSet<>();
+
+        // base case for 1 size array, there will be no dupe
+        if (nums.length == 1){
+            return false;
+        }
+
+        for (int i = 0; i < nums.length; i++){
+            // if we find our element in the sliding window, we found our dupe that meets our needs
+            // if not we add to our sliding window
+            if (slidingWindow.contains(nums[i])){
+                return true;
+            } else{
+                slidingWindow.add(nums[i]);
+            }
+
+            //update sliding window if needed
+            if(slidingWindow.size() > k){
+                slidingWindow.remove(nums[i - k]);
+            }
+        }
+
+        return false;
     }
 }
